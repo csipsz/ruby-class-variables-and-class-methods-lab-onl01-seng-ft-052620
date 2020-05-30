65d86679a2a973975e42e6c4d2f2801e574a757e
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-30 02:56:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,5 +38,17 @@ class Song
     hash
   end 
   
+  def self.artist_count 
+    hash = {}
+    @@artists.each do |artist|
+      if !hash[artist]
+        hash[artist] = 1 
+      else 
+        hash[artist] += 1
+      end 
+    end 
+    hash
+  end 
+  
   
 end 
